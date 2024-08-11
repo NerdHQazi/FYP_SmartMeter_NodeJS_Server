40cd25194d4f2a32d6de3e2c919a3db953de1bc0
Dare Harry <harrymrpotter360@gmail.com>
2024-08-11 23:51:24 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+import contractAbi from "./SmartMeterContractABI.json"
+const Web3 = require('web3');
+const web3 = new Web3(process.env.INFURA_URL);
+
+const contractABI = contractAbi;
+const contractAddress = '0x6030F493594e735C0A68012FdD19bC37C65cCeB0';
+
+const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+contract.events.EnergyUnitsPurchased({
+  fromBlock: 'latest'
+}, (error, event) => {
+  if (error) {
+      console.error('Error:', error);
+  } else {
+      console.log('Event:', event);
+      // Handle the event data here
+  }
+});
+
+
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
